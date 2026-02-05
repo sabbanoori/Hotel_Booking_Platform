dc7eb45b1bab7e37c6c1a1c60b19cd4f466dd618
sabba noori <sabbanoori1921@gmail.com>
2026-02-05 12:59:06 +0530

    db-name-change

--- server/configs/db.js
+++ server/configs/db.js
@@ -4,7 +4,7 @@ const connectDB = async () => {
 
     try {
         mongoose.connection.on('connected', () => console.log("Database Connected"));
-        await mongoose.connect(`${process.env.MONGODB_URI}/hotel-booking`);
+        await mongoose.connect(`${process.env.MONGODB_URI}/HotelBooking`);
     } catch (error) {
         console.error(error.message);
     }
